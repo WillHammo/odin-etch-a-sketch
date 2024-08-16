aca67e9950ae6d793372ad01a95177f02c4cacb7
WillHammo <167308794+WillHammo@users.noreply.github.com>
2024-08-16 15:49:18 +0100

    Fix: Remove test content loading into grid rows

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ function playGame(){
         for(let i = 0; i < area; i++){
             const gridRow = document.createElement('div');
             gridRow.classList.add('flex-row');
-            gridRow.textContent = "test";
             gridContainer.appendChild(gridRow);
             for(let j = 0; j < area; j++){
                 const gridCol = document.createElement('div');
                 gridCol.classList.add('flex-col');
-                gridCol.textContent = "test";
+                gridCol.textContent = "Content";
                 gridRow.appendChild(gridCol);
             }
         }
     }
+
+    function changeTileColor(tile){
+
+    }
+
+
     createGrid(4);
 
 }
